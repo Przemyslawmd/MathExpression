1ca510abcd058a2937f9a6b38de3cf18caea0498
przemyslawmd <przemyslawmd@gmail.com>
2021-12-06 04:06:04 +0100

    [Parser] Token for negative numbers added - missed enum for previous commit

--- Tokens/Token.py
+++ Tokens/Token.py
@@ -3,25 +3,33 @@ from enum import Enum
 
 
 class TokenValue(Enum):
+
+    # Token of type NONE
     NONE = 0
 
+    # Tokens of type OPERATION
     PLUS = 1
     MINUS = 2
     MULTIPLICATION = 3
     DIVISION = 4
 
+    # Tokens of type TRIGONOMETRY
     SINE = 5
     COSINE = 6
     TANGENT = 7
     COTANGENT = 8
 
-    POWER = 9
-    ROOT = 10
-    LOG = 11
-    X = 12
+    # Tokens of type OTHER
+    LOG = 9
+    NEGATIVE = 10
+    POWER = 11
+    ROOT = 12
+    X = 13
 
-    BRACKET_LEFT = 13
-    BRACKET_RIGHT = 14
+    # Tokens of type BRACKET
+    BRACKET_LEFT = 14
+    BRACKET_RIGHT = 15
+
 
 
 class TokenType(Enum):
