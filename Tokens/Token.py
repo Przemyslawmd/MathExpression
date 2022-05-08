7a9451d213ac587dcd775a35350eafe341115943
przemyslawmd <przemyslawmd@gmail.com>
2022-05-08 22:42:17 +0200

    Unnecessary comments removed

--- Tokens/Token.py
+++ Tokens/Token.py
@@ -4,22 +4,18 @@ from enum import Enum
 
 class TokenValue(Enum):
 
-    # Token of type NUMBER
     NUMBER = 0
 
-    # Tokens of type OPERATION
     PLUS = 1
     MINUS = 2
     MULTIPLICATION = 3
     DIVISION = 4
 
-    # Tokens of type TRIGONOMETRY
     SINE = 5
     COSINE = 6
     TANGENT = 7
     COTANGENT = 8
 
-    # Tokens of type OTHER
     LOG = 9
     NEGATIVE = 10
     POWER = 11
@@ -27,7 +23,6 @@ class TokenValue(Enum):
     X = 13
     X_NEGATIVE = 14
 
-    # Tokens of type BRACKET
     BRACKET_LEFT = 15
     BRACKET_RIGHT = 16
     BRACKET_SQUARE_LEFT = 17
