c275e51d4a90e9a9fe5ea063c058e29d1846a97f
przemyslawmd <przemyslawmd@gmail.com>
2023-03-01 01:36:29 +0100

    Color attributes in namedtuple

--- color.py
+++ color.py
@@ -1,5 +1,6 @@
 
 from enum import Enum
+from collections import namedtuple
 
 
 class Color(Enum):
@@ -14,22 +15,19 @@ class Color(Enum):
     WHITE = 8
 
 
-class ColorData:
-    def __init__(self, rgb, text):
-        self.rgb = rgb
-        self.text = text
+ColorAttr = namedtuple('ColorAttr', ('rgb', 'text'))
 
 
 Colors = {
-    Color.BLACK:       ColorData((0, 0, 0), "Black"),
-    Color.BLUE:        ColorData((0, 0, 255), "Blue"),
-    Color.GREEN:       ColorData((0, 128, 0), "Green"),
-    Color.LIGHT_BLUE:  ColorData((0, 191, 255), "Light Blue"),
-    Color.LIGHT_GREEN: ColorData((0, 255, 128), "Light Green"),
-    Color.ORANGE:      ColorData((255, 140, 0), "Orange"),
-    Color.RED:         ColorData((255, 0, 0), "Red"),
-    Color.YELLOW:      ColorData((255, 255, 0), "Yellow"),
-    Color.WHITE:       ColorData((255, 255, 255), "White"),
+    Color.BLACK:       ColorAttr((0, 0, 0), 'Black'),
+    Color.BLUE:        ColorAttr((0, 0, 255), 'Blue'),
+    Color.GREEN:       ColorAttr((0, 128, 0), 'Green'),
+    Color.LIGHT_BLUE:  ColorAttr((0, 191, 255), 'Light Blue'),
+    Color.LIGHT_GREEN: ColorAttr((0, 255, 128), 'Light Green'),
+    Color.ORANGE:      ColorAttr((255, 140, 0), 'Orange'),
+    Color.RED:         ColorAttr((255, 0, 0), 'Red'),
+    Color.YELLOW:      ColorAttr((255, 255, 0), 'Yellow'),
+    Color.WHITE:       ColorAttr((255, 255, 255), 'White'),
 }
 
 
