a6fc8aac90d97e2f1ac1a46700d1d8e8c5616e08
przemyslawmd <przemyslawmd@gmail.com>
2023-04-03 02:25:34 +0200

    Option for graph labels in setting window

--- windowSettings.py
+++ windowSettings.py
@@ -18,37 +18,47 @@ class WindowSettings(QDialog):
 
         layout_grid = QGridLayout()
 
-        layout_grid.addWidget(QLabel("X Grid"), 1, 1)
+        row = 1
+        layout_grid.addWidget(QLabel("X Grid"), row, 1)
         self.check_x_grid = QCheckBox()
         self.check_x_grid.setChecked(settings.x_grid)
-        layout_grid.addWidget(self.check_x_grid, 1, 2, alignment=QtCore.Qt.AlignRight)
+        layout_grid.addWidget(self.check_x_grid, row, 2, alignment=QtCore.Qt.AlignRight)
 
-        layout_grid.addWidget(QLabel("Y Grid"), 2, 1)
+        row += 1
+        layout_grid.addWidget(QLabel("Y Grid"), row, 1)
         self.check_y_grid = QCheckBox()
         self.check_y_grid.setChecked(settings.y_grid)
-        layout_grid.addWidget(self.check_y_grid, 2, 2, alignment=QtCore.Qt.AlignRight)
+        layout_grid.addWidget(self.check_y_grid, row, 2, alignment=QtCore.Qt.AlignRight)
 
-        layout_grid.addWidget(QLabel("Display coordinates"), 3, 1)
+        row += 1
+        layout_grid.addWidget(QLabel("Display Coordinates"), row, 1)
         self.coordinates = QCheckBox()
         self.coordinates.setChecked(self.settings.coordinates)
-        layout_grid.addWidget(self.coordinates, 3, 2, alignment=QtCore.Qt.AlignRight)
+        layout_grid.addWidget(self.coordinates, row, 2, alignment=QtCore.Qt.AlignRight)
 
-        layout_grid.addWidget(QLabel("Precision"), 4, 1)
+        row += 1
+        layout_grid.addWidget(QLabel("Graph Label"), row, 1)
+        self.coordinates = QCheckBox()
+        self.coordinates.setChecked(self.settings.coordinates)
+        layout_grid.addWidget(self.coordinates, row, 2, alignment=QtCore.Qt.AlignRight)
+
+        row += 1
+        layout_grid.addWidget(QLabel("Precision"), row, 1)
         self.insert_precision = QComboBox()
         self.insert_precision.setMaximumWidth(100)
         for i, precision in enumerate((0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0)):
             self.insert_precision.addItem(str(precision))
             if precision == self.settings.precision:
                 self.insert_precision.setCurrentIndex(i)
-        layout_grid.addWidget(self.insert_precision, 4, 2, alignment=QtCore.Qt.AlignRight)
+        layout_grid.addWidget(self.insert_precision, row, 2, alignment=QtCore.Qt.AlignRight)
 
-        layout_grid.addWidget(QLabel("Background color"), 5, 1)
+        row += 1
+        layout_grid.addWidget(QLabel("Background Color"), row, 1)
         self.background = QComboBox()
         self.background.setMaximumWidth(120)
-
         [self.background.addItem(value.text) for value in Colors.values()]
         self.background.setCurrentText(Colors[self.settings.background].text)
-        layout_grid.addWidget(self.background, 5, 2, alignment=QtCore.Qt.AlignRight)
+        layout_grid.addWidget(self.background, row, 2, alignment=QtCore.Qt.AlignRight)
 
         for row in range(1, layout_grid.rowCount()):
             layout_grid.setRowMinimumHeight(row, 30)
@@ -64,7 +74,7 @@ class WindowSettings(QDialog):
 
         layout.addLayout(layout_button)
         self.setMinimumWidth(320)
-        self.setMinimumHeight(280)
+        self.setMinimumHeight(320)
         self.setWindowModality(Qt.ApplicationModal)
         self.setLayout(layout)
         self.setGeometry(550, 500, 0, 0)
