393b1451323c82abe6c6997b243c6ffbe33c1bad
przemyslawmd <przemyslawmd@gmail.com>
2024-10-26 00:02:54 +0200

    Style change

--- gui/windowSettings.py
+++ gui/windowSettings.py
@@ -14,49 +14,44 @@ class WindowSettings(QDialog):
         self.parent = parent
         layout = QVBoxLayout()
         layout.setContentsMargins(30, 20, 30, 0)
+
         layout_grid = QGridLayout()
 
-        row = 1
-        layout_grid.addWidget(QLabel("X Grid"), row, 1)
+        layout_grid.addWidget(QLabel("X Grid"), 1, 1)
         self.check_x_grid = QCheckBox()
         self.check_x_grid.setChecked(settings.x_grid)
-        layout_grid.addWidget(self.check_x_grid, row, 2, alignment=QtCore.Qt.AlignRight)
+        layout_grid.addWidget(self.check_x_grid, 1, 2, alignment=QtCore.Qt.AlignRight)
 
-        row += 1
-        layout_grid.addWidget(QLabel("Y Grid"), row, 1)
+        layout_grid.addWidget(QLabel("Y Grid"), 2, 1)
         self.check_y_grid = QCheckBox()
         self.check_y_grid.setChecked(settings.y_grid)
-        layout_grid.addWidget(self.check_y_grid, row, 2, alignment=QtCore.Qt.AlignRight)
+        layout_grid.addWidget(self.check_y_grid, 2, 2, alignment=QtCore.Qt.AlignRight)
 
-        row += 1
-        layout_grid.addWidget(QLabel("Display Coordinates"), row, 1)
+        layout_grid.addWidget(QLabel("Display Coordinates"), 3, 1)
         self.check_coordinates = QCheckBox()
         self.check_coordinates.setChecked(settings.coordinates)
-        layout_grid.addWidget(self.check_coordinates, row, 2, alignment=QtCore.Qt.AlignRight)
+        layout_grid.addWidget(self.check_coordinates, 3, 2, alignment=QtCore.Qt.AlignRight)
 
-        row += 1
-        layout_grid.addWidget(QLabel("Graph Label"), row, 1)
+        layout_grid.addWidget(QLabel("Graph Label"), 4, 1)
         self.check_label = QCheckBox()
         self.check_label.setChecked(settings.graph_label)
-        layout_grid.addWidget(self.check_label, row, 2, alignment=QtCore.Qt.AlignRight)
+        layout_grid.addWidget(self.check_label, 4, 2, alignment=QtCore.Qt.AlignRight)
 
-        row += 1
-        layout_grid.addWidget(QLabel("Precision"), row, 1)
+        layout_grid.addWidget(QLabel("Precision"), 5, 1)
         self.combo_precision = QComboBox()
         self.combo_precision.setMaximumWidth(100)
         for i, precision in enumerate((0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0)):
             self.combo_precision.addItem(str(precision))
             if precision == settings.precision:
                 self.combo_precision.setCurrentIndex(i)
-        layout_grid.addWidget(self.combo_precision, row, 2, alignment=QtCore.Qt.AlignRight)
+        layout_grid.addWidget(self.combo_precision, 5, 2, alignment=QtCore.Qt.AlignRight)
 
-        row += 1
-        layout_grid.addWidget(QLabel("Background Color"), row, 1)
+        layout_grid.addWidget(QLabel("Background Color"), 6, 1)
         self.combo_background = QComboBox()
         self.combo_background.setMaximumWidth(120)
         [self.combo_background.addItem(value.text) for value in Colors.values()]
         self.combo_background.setCurrentText(Colors[settings.background].text)
-        layout_grid.addWidget(self.combo_background, row, 2, alignment=QtCore.Qt.AlignRight)
+        layout_grid.addWidget(self.combo_background, 6, 2, alignment=QtCore.Qt.AlignRight)
 
         for row in range(1, layout_grid.rowCount()):
             layout_grid.setRowMinimumHeight(row, 30)
