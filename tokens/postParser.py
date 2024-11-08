81fa4ab8d8d4e07d0fa1a350b174471d31c5b24f
przemyslawmd <przemyslawmd@gmail.com>
2024-11-08 23:50:16 +0100

    [PostParser]: Refactoring

--- tokens/postParser.py
+++ tokens/postParser.py
@@ -1,4 +1,6 @@
 
+from collections import deque
+
 from tokens.token import Token, TokenType
 from tokens.tokenGroup import TokenGroup
 
@@ -27,36 +29,34 @@ def add_multiplication_tokens(tokens):
     for index, token in enumerate(tokens[:-1]):
         if token.type in left_multiplication and tokens[index + 1].type in right_multiplication:
             indices.append(index + 1)
-    index_shift = 0
-    for index in indices:
-        tokens.insert(index + index_shift, Token(TokenType.MULTIPLICATION))
-        index_shift += 1
+    for index in reversed(indices):
+        tokens.insert(index, Token(TokenType.MULTIPLICATION))
 
 
 def remove_angle_brackets(tokens) -> bool:
-    tokens_to_remove = []
+    indices = []
     for index, token in enumerate(tokens):
         if token.type is not TokenType.BRACKET_ANGLE_LEFT:
             continue
         if index == 0:
             return False
-        if tokens[index - 1].type not in [TokenType.ROOT, TokenType.LOG]:
+        if tokens[index - 1].type not in (TokenType.ROOT, TokenType.LOG):
             return False
         if tokens[index + 1].type is not TokenType.NUMBER:
             return False
         if tokens[index + 2].type is not TokenType.BRACKET_ANGLE_RIGHT:
             return False
         tokens[index - 1].data = tokens[index + 1].data
-        tokens_to_remove.extend([index, index + 1, index + 2])
+        indices.extend([index, index + 1, index + 2])
 
-    for i in range(len(tokens) - 1, -1, -1):
-        if i in tokens_to_remove:
-            del tokens[i]
+    for index in reversed(indices):
+        del tokens[index]
     return True
 
 
 def remove_negative_tokens(tokens) -> bool:
     is_negative = False
+    indices = deque()
     for index, token in enumerate(tokens):
         if token.type is TokenType.NEGATIVE:
             is_negative = True
@@ -66,12 +66,16 @@ def remove_negative_tokens(tokens) -> bool:
                 tokens[index].data *= -1
             elif token.type is TokenType.X:
                 tokens[index] = Token(TokenType.X_NEGATIVE)
-            elif token.type in [TokenType.BRACKET_LEFT, TokenType.ROOT, TokenType.LOG, TokenGroup.trigonometry]:
-                tokens.insert(index, Token(TokenType.MULTIPLICATION))
-                tokens.insert(index, Token(TokenType.NUMBER, -1))
+            elif token.type in (TokenType.BRACKET_LEFT, TokenType.ROOT, TokenType.LOG, TokenGroup.trigonometry):
+                indices.append(index)
             else:
                 return False
             is_negative = False
+
+    while indices:
+        index = indices.pop()
+        tokens.insert(index, Token(TokenType.MULTIPLICATION))
+        tokens.insert(index, Token(TokenType.NUMBER, -1))
 
     for i in range(len(tokens) - 1, -1, -1):
         if tokens[i].type is TokenType.NEGATIVE:
