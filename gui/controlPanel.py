ebf3d355629edd8ab659e9b26ab46af19e798ed9
przemyslawmd <przemyslawmd@gmail.com>
2024-11-08 18:58:08 +0100

    Style changes

--- gui/controlPanel.py
+++ gui/controlPanel.py
@@ -13,13 +13,11 @@ def create_button(label, width, func=None):
     return button
 
 
-def create_layout_for_widget_and_label(widget, width, text, text_width, alignment=None, default_value=""):
+def create_widget_layout(widget, width, text, text_width, default_value=None):
     widget.setMinimumWidth(width)
     widget.setMaximumWidth(width)
     if isinstance(widget, QLineEdit):
         widget.setText(default_value)
-    if alignment is not None:
-        widget.setAlignment(alignment)
     label = QLabel(text)
     label.setFixedSize(text_width, 10)
     label.setMargin(0)
@@ -39,7 +37,7 @@ class ControlPanel:
         self.y_max = QLineEdit()
         self.coordinates = QLineEdit()
         self.pen_width = QComboBox()
-        self.box_pen_colors = QComboBox()
+        self.pen_colors = QComboBox()
         self.current_pen_color = (0, 255, 128)
         self.ratio_slider = QSlider()
         self.ratio_label = QLabel()
@@ -48,27 +46,23 @@ class ControlPanel:
         )
 
 
-    def on_box_pen_colors_changed(self, color_text):
-        self.current_pen_color = next((color.rgb for color in Colors.values() if color.text == color_text), None)
-
-
     def create_first_row(self, layout, func_draw, func_append, x_min, x_max):
         layout.setContentsMargins(0, 0, 0, 0)
         layout.addWidget(create_button("Draw Graph", 140, func_draw), 0, 0)
         layout.addWidget(create_button("Append Graph", 140, func_append), 0, 1)
         layout.setSpacing(15)
 
-        widget_with_label = create_layout_for_widget_and_label(self.x_min, 40, "X Min", 40, Qt.AlignCenter, str(x_min))
-        layout.addLayout(widget_with_label, 0, 3)
+        widget_x_min = create_widget_layout(self.x_min, 40, "X Min", 40, str(x_min))
+        layout.addLayout(widget_x_min, 0, 3)
 
-        widget_with_label = create_layout_for_widget_and_label(self.x_max, 40, "X Max", 40, Qt.AlignCenter, str(x_max))
-        layout.addLayout(widget_with_label, 0, 4)
+        widget_x_max = create_widget_layout(self.x_max, 40, "X Max", 40, str(x_max))
+        layout.addLayout(widget_x_max, 0, 4)
 
-        [self.pen_width.addItem(str(x))
-         for x in (0.1, 0.2, 0.3, 0.4, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 7, 8, 9, 10)]
+        for x in (0.1, 0.2, 0.3, 0.4, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 7, 8, 9, 10):
+            self.pen_width.addItem(str(x))
         self.pen_width.setCurrentIndex(4)
-        widget_with_label = create_layout_for_widget_and_label(self.pen_width, 100, "Line Width", 65)
-        layout.addLayout(widget_with_label, 0, 6)
+        widget_pen_width = create_widget_layout(self.pen_width, 100, "Line Width", 65)
+        layout.addLayout(widget_pen_width, 0, 6)
 
         layout_ratio = QHBoxLayout()
         layout_ratio.setSpacing(8)
@@ -83,12 +77,12 @@ class ControlPanel:
         ratio = self.ratio_values[self.ratio_slider.value()]
         self.ratio_label.setText(str(ratio))
 
-        button = create_button("Default", 80, lambda: self.ratio_slider.setValue(11))
+        button_ratio = create_button("Default", 80, lambda: self.ratio_slider.setValue(11))
 
         layout_ratio.addWidget(label)
         layout_ratio.addWidget(self.ratio_slider)
         layout_ratio.addWidget(self.ratio_label)
-        layout_ratio.addWidget(button)
+        layout_ratio.addWidget(button_ratio)
 
         layout.addLayout(layout_ratio, 0, 8)
 
@@ -97,24 +91,28 @@ class ControlPanel:
         layout.addWidget(create_button("Clear Insert Area", 140, func_clear_insert), 1, 0)
         layout.addWidget(create_button("Clear Plot Area", 140, func_clear_plot), 1, 1)
 
-        widget_with_label = create_layout_for_widget_and_label(self.y_min, 40, "Y Min", 40, Qt.AlignCenter)
-        layout.addLayout(widget_with_label, 1, 3)
+        widget_y_min = create_widget_layout(self.y_min, 40, "Y Min", 40)
+        layout.addLayout(widget_y_min, 1, 3)
 
-        widget_with_label = create_layout_for_widget_and_label(self.y_max, 40, "Y Max", 40, Qt.AlignCenter)
-        layout.addLayout(widget_with_label, 1, 4)
+        widget_y_max = create_widget_layout(self.y_max, 40, "Y Max", 40)
+        layout.addLayout(widget_y_max, 1, 4)
 
-        [self.box_pen_colors.addItem(value.text) for value in Colors.values()]
-        self.box_pen_colors.setCurrentIndex(4)
-        self.box_pen_colors.currentTextChanged.connect(self.on_box_pen_colors_changed)
-        widget_with_label = create_layout_for_widget_and_label(self.box_pen_colors, 100, "Line Color", 65)
-        layout.addLayout(widget_with_label, 1, 6)
+        for color in Colors.values():
+            self.pen_colors.addItem(color.text)
+        self.pen_colors.setCurrentIndex(4)
+        self.pen_colors.currentTextChanged.connect(self.on_pen_colors_changed)
+        widget_pen_color = create_widget_layout(self.pen_colors, 100, "Line Color", 65)
+        layout.addLayout(widget_pen_color, 1, 6)
 
-        widget_with_label = create_layout_for_widget_and_label(self.coordinates, 420, "Coordinate", 70, Qt.AlignRight)
-        widget_with_label.setAlignment(Qt.AlignLeft)
-        layout.addLayout(widget_with_label, 1, 8)
+        widget_coordinates = create_widget_layout(self.coordinates, 420, "Coordinate", 70)
+        layout.addLayout(widget_coordinates, 1, 8)
 
 
     def connect_slider(self, func):
         self.ratio_slider.valueChanged.connect(func)
 
+    # ------------------------------- INTERNAL ----------------------------------- #
+
+    def on_pen_colors_changed(self, color_text):
+        self.current_pen_color = next((color.rgb for color in Colors.values() if color.text == color_text), None)
 
