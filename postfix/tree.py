13d058a9bd6a32e6f36e90302bff00bc61a3b04b
Przemyslaw Madej <przemyslawmd@gmail.com>
2025-10-05 19:29:44 +0200

    One operand actions in the tree

--- postfix/tree.py
+++ postfix/tree.py
@@ -5,16 +5,27 @@ from postfix.node import Node
 from tokens.token import TokenType
 
 
+one_operand = (TokenType.COSINE,
+               TokenType.COTANGENT,
+               TokenType.SINE,
+               TokenType.TANGENT,
+               TokenType.LOG)
+
 def create_tree(tokens):
     stack = deque()
     for token in tokens:
         if token.type is TokenType.NUMBER or token.type is TokenType.X:
             stack.append(token)
+        elif token.type in one_operand:
+            operand = stack.pop()
+            left = operand if isinstance(operand, Node) else Node(operand)
+            node = Node(token, left)
+            stack.append(node)
         else:
-            token_1 = stack.pop()
-            token_2 = stack.pop()
-            right = token_1 if isinstance(token_1, Node) else Node(token_1)
-            left = token_2 if isinstance(token_2, Node) else Node(token_2)
+            operand_1 = stack.pop()
+            operand_2 = stack.pop()
+            right = operand_1 if isinstance(operand_1, Node) else Node(operand_1)
+            left = operand_2 if isinstance(operand_2, Node) else Node(operand_2)
             node = Node(token, left, right)
             stack.append(node)
     return stack.pop()
--- test/testTree.py
+++ test/testTree.py
@@ -10,9 +10,15 @@ from postfix.postfix import Postfix
 
 class TestTree(TestCase):
 
-    def test_basic_1(self):
+    def test_tree_1(self):
         tokens = Parser("(2 + x)(x - 4)").parse()
         postfix = Postfix().create_postfix(tokens)
         root = create_tree(postfix)
         check_tree(root, postfix)
 
+
+    def test_tree_2(self):
+        tokens = Parser("10tgx - xctgx").parse()
+        postfix = Postfix().create_postfix(tokens)
+        root = create_tree(postfix)
+        check_tree(root, postfix)
