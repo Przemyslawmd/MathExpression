b93ef4d65e811618f85f68049526ba73abbc48c7
Przemyslaw Madej <przemyslawmd@gmail.com>
2025-10-08 23:05:58 +0200

    Tests for incorrect logarithm base

--- test/testValidator.py
+++ test/testValidator.py
@@ -13,43 +13,64 @@ class TestParserError(TestCase):
 
 
     def test_validator_bracket_empty_beginning(self):
-        Parser("()").parse()
+        tokens = Parser("()").parse()
         errors = ErrorStorage.get_errors()
+        self.assertIsNone(tokens)
         self.assertEqual(errors[0], Message[Error.VALIDATOR_BRACKET_LEFT])
 
 
     def test_validator_bracket_empty(self):
-        Parser("5x + tg() + 34").parse()
+        tokens = Parser("5x + tg() + 34").parse()
         errors = ErrorStorage.get_errors()
+        self.assertIsNone(tokens)
         self.assertEqual(errors[0], Message[Error.VALIDATOR_BRACKET_LEFT])
 
 
     def test_validator_arithmetic(self):
-        Parser("5x + sin(x + )").parse()
+        tokens = Parser("5x + sin(x + )").parse()
         errors = ErrorStorage.get_errors()
+        self.assertIsNone(tokens)
         self.assertEqual(errors[0], Message[Error.VALIDATOR_ARITHMETIC])
 
 
     def test_validator_invalid_last_symbol_1(self):
-        Parser("5x + cosx +").parse()
+        tokens = Parser("5x + cosx +").parse()
         errors = ErrorStorage.get_errors()
+        self.assertIsNone(tokens)
         self.assertEqual(errors[0], Message[Error.VALIDATOR_LAST_TOKEN])
 
 
     def test_validator_invalid_last_symbol_2(self):
-        Parser("sin").parse()
+        tokens = Parser("sin").parse()
         errors = ErrorStorage.get_errors()
+        self.assertIsNone(tokens)
         self.assertEqual(errors[0], Message[Error.VALIDATOR_LAST_TOKEN])
 
 
     def test_validator_invalid_first_symbol_1(self):
-        Parser("*12").parse()
+        tokens = Parser("*12").parse()
         errors = ErrorStorage.get_errors()
+        self.assertIsNone(tokens)
         self.assertEqual(errors[0], Message[Error.VALIDATOR_FIRST_TOKEN])
 
 
     def test_validator_invalid_first_symbol_2(self):
-        Parser("/").parse()
+        tokens = Parser("/").parse()
         errors = ErrorStorage.get_errors()
+        self.assertIsNone(tokens)
         self.assertEqual(errors[0], Message[Error.VALIDATOR_FIRST_TOKEN])
+
+
+    def test_validator_logarithm_base_0(self):
+        tokens = Parser("sinx + log<1>x").parse()
+        errors = ErrorStorage.get_errors()
+        self.assertIsNone(tokens)
+        self.assertEqual(errors[0], Message[Error.VALIDATOR_LOGARITHM_BASE])
+
+
+    def test_validator_logarithm_base_1(self):
+        tokens = Parser("12x^2 - log<0>10").parse()
+        errors = ErrorStorage.get_errors()
+        self.assertIsNone(tokens)
+        self.assertEqual(errors[0], Message[Error.VALIDATOR_LOGARITHM_BASE])
 
